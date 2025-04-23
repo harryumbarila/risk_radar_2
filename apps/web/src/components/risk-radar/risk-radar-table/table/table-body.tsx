28437040c2fd078833aacbb3ff1f86142ea7a890
lockin-solvedex <enderson@solvedex.com>
2025-04-23 14:22:09 -0400

    refactor: 💡 change the font and color of letter in light/dark
    ✅ Closes: JOBOX_1646

--- apps/web/src/components/risk-radar/risk-radar-table/table/table-body.tsx
+++ apps/web/src/components/risk-radar/risk-radar-table/table/table-body.tsx
@@ -118,14 +118,14 @@ export const TableBody: FC<Props> = ({
 
   return (
     <div className="overflow-x-auto">
-      <table className="datatable-table w-full table-auto !border-collapse break-words px-4 md:px-8 align-middle">
+      <table className="datatable-table w-full table-auto !border-collapse break-words px-4 md:px-8 align-middle text-black dark:text-white">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
                 <th key={header.id}>
                   <div className="flex items-center">
-                    <span>
+                    <span className="font-bold">
                       {header.isPlaceholder
                         ? null
                         : renderHeader(header.getContext())}
