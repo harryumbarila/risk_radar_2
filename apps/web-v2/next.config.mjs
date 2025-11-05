82e99aa33ac5c939f5ebdc9cd6a606f6981cdad2
Crhistian Caraballo <cristianjcj35@gmail.com>
2025-11-05 09:21:30 -0500

    fix: fixed based path

--- apps/web-v2/next.config.mjs
+++ apps/web-v2/next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   basePath: '/dashboard-v2/dashboard',
   experimental: {
